6a9e1d522db97f9c79f943ab27b9e1de71137a8b
Jihae Han <Jihae.Han@city.ac.uk>
2020-08-19 12:26:18 +0100

    menu spacing and alignment successfully added

--- YoutubeRPG/ConversationScene.cs
+++ YoutubeRPG/ConversationScene.cs
@@ -10,18 +10,18 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace YoutubeRPG
 {
-    public class Menu
+    public class ConversationScene
     {
-        public event EventHandler OnMenuChanged; //type of delegate, which calls an 'event'
+        public event EventHandler OnConversationChanged; //type of delegate, which calls an 'event'
 
         public string Axis, Effects;
-        public Vector2 Alignment, Spacing;
+        public Vector2 Alignment, Spacing; 
         [XmlElement("Item")]
-        public List<MenuItem> Items;
+        public List<ConversationItem> Items;
         int itemNumber;
         string id;
 
-        
+
         public int ItemNumber
         {
             get { return itemNumber; }
@@ -29,17 +29,17 @@ namespace YoutubeRPG
         }
 
         public string ID
-        { 
+        {
             get { return id; }
             set
             {
                 id = value;
-                OnMenuChanged(this, null);
+                OnConversationChanged(this, null);
             }
         }
         public void Transition(float alpha)
         {
-            foreach(MenuItem item in Items)
+            foreach (ConversationItem item in Items)
             {
                 item.Image.IsActive = true;
                 item.Image.Alpha = alpha;
@@ -53,14 +53,14 @@ namespace YoutubeRPG
         void AlignMenuItems()
         {
             Vector2 dimensions = Vector2.Zero;
-            foreach (MenuItem item in Items)
+            foreach (ConversationItem item in Items)
                 dimensions += new Vector2(item.Image.SourceRect.Width,
                     item.Image.SourceRect.Height);
 
             dimensions = new Vector2((ScreenManager.Instance.Dimensions.X -
                 dimensions.X) / 2, (ScreenManager.Instance.Dimensions.Y - dimensions.Y) / 2);
 
-            foreach (MenuItem item in Items)
+            foreach (ConversationItem item in Items)
             {
                 if (Axis == "X")
                     item.Image.Position = new Vector2(dimensions.X,
@@ -73,21 +73,21 @@ namespace YoutubeRPG
             }
         }
 
-        public Menu()
+        public ConversationScene()
         {
             id = String.Empty;
             itemNumber = 0;
             Effects = String.Empty;
             Axis = "Y";
-            Items = new List<MenuItem>();
             Alignment = Vector2.Zero;
             Spacing = Vector2.Zero;
+            Items = new List<ConversationItem>();
         }
 
         public void LoadContent()
         {
             string[] split = Effects.Split(':');
-            foreach(MenuItem item in Items)
+            foreach (ConversationItem item in Items)
             {
                 item.Image.LoadContent();
                 foreach (string s in split)
@@ -97,21 +97,21 @@ namespace YoutubeRPG
         }
         public void UnloadContent()
         {
-            foreach (MenuItem item in Items)
+            foreach (ConversationItem item in Items)
                 item.Image.UnloadContent();
         }
         public void Update(GameTime gameTime)
         {
-            if (itemNumber < 0) 
-                itemNumber = 0; 
-            else if (itemNumber > Items.Count - 1) 
-                itemNumber = Items.Count - 1; 
+            if (itemNumber < 0)
+                itemNumber = 0;
+            else if (itemNumber > Items.Count - 1)
+                itemNumber = Items.Count - 1;
 
             for (int i = 0; i < Items.Count; i++)
             {
-                if (i == itemNumber) 
-                    Items[i].Image.IsActive = true; 
-                else 
+                if (i == itemNumber)
+                    Items[i].Image.IsActive = true;
+                else
                     Items[i].Image.IsActive = false;
 
                 Items[i].Image.Update(gameTime);
@@ -119,7 +119,7 @@ namespace YoutubeRPG
         }
         public void Draw(SpriteBatch spriteBatch)
         {
-            foreach (MenuItem item in Items)
+            foreach (ConversationItem item in Items)
                 item.Image.Draw(spriteBatch);
         }
     }
